da5f4c58ebede52a0826bbdecc64eebe59bd20d2
Code <taste397@naver.com>
2022-03-07 10:48:12 +0900

    commit

--- app/src/main/java/com/example/myapplication/ApplicationActivity1.kt
+++ app/src/main/java/com/example/myapplication/ApplicationActivity1.kt
@@ -1,11 +1,24 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
 
 class ApplicationActivity1 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_application1)
+
+        findViewById<TextView>(R.id.changeActivity).setOnClickListener {
+            startActivity(Intent(this, ApplicationActivity2::class.java))
+        }
+
+        findViewById<TextView>(R.id.textMethod).setOnClickListener {
+            (applicationContext as MasterApplication).methodFromApplication()
+            val userId = (applicationContext as MasterApplication).id
+            Log.d("testt", "userId : " + userId)
+        }
     }
 }
